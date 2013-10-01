b6549ac895a960c1889afc58f49f71b1c63a8549
mirza9252 <mirza9252@gmail.com>
2013-10-01 12:26:03 +0600

    Fixed the Bug

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <body style="text-align: center ">
         <h1 > User Profile For HRM Tariner </h1><br/>
         <a href="index.php">Create User!</a>
-        <a href="view.php.php">View All User Profile!</a>
+        <a href="view.php">View All User Profile!</a>
         <a href="update.php">Update User Profile!</a>
         <a href="delete.php">Deleate Prfile!</a>
         
